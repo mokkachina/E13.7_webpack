39ed1deb5369353163f9759f46132711c943c9e4
Maksim Klimov <maxiklim80@gmail.com>
2023-12-09 20:03:54 +0600

    webpack not create dandle with web.js

---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './src/index.js',
+        web: './src/web.js',
+
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
