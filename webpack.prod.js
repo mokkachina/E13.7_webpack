202df480771526e67f3c7620af0712605db35c66
Maksim Klimov <maxiklim80@gmail.com>
2023-12-13 18:51:49 +0600

    edit prod off HMR

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,26 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
-
+    devServer: {
+        static: './dist',
+        hot: false,
+    },
+    plugins: [
+        // Добавлен MiniCssExtractPlugin
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
 });
